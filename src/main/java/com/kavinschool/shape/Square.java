80707c86770caeb0da249ab851f9446ea229915a
Kangs <>
2020-05-05 06:55:37 -0700

    Added without test cases

---
+++ src/main/java/com/kavinschool/shape/Square.java
@@ -0,0 +1,42 @@
+package com.kavinschool.shape;
+
+public class Square extends Shape2D {
+
+    double side;
+	
+    static final String NAME = "Square";
+	
+    Square(int s) {
+        side = s;
+        count();
+        count2D();
+        assignUniqueID();
+    }
+
+    public double perimeter() {
+        return 4 * side;
+    }
+
+    public double area() {
+        return side * side;
+    }
+
+    public void draw() {
+        System.out.println("Name:" + NAME);
+        System.out.println("Type: " + type);
+        System.out.println("ID: " + id);
+        System.out.println("Side: " + side);
+    }
+
+    protected void count() {
+        Square.totalShapes++;
+    }
+
+    protected void count2D() {
+        Shape2D.total2dShapes++;
+    }
+
+    protected void assignUniqueID() {
+        id = Shape.totalShapes;
+    }
+}
